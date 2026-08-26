d051ed073fe3d38461368a76229ab2d6111131c1
Davi <davibeber.rbr@gmail.com>
2026-08-25 21:23:22 -0300

    Atualizações do portifolio e menu mobile funcionando

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+
+const btn =document.getElementById("btn");
+
+const menuMobile = document.getElementById("menu_mobile");
+
+btn.addEventListener("click", () => {
+  menuMobile.classList.toggle("ativo");
+
+});
