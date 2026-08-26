d4ec3e8a5157b94ba5fd29bb56a67ecb41935b91
Davi <davibeber.rbr@gmail.com>
2026-08-25 22:54:34 -0300

    Menu mobile funcionando, porem não terminado

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const btn =document.getElementById("btn");
 
 const menuMobile = document.getElementById("menu_mobile");
 
+const list = document.getElementById("list");
+
+const close = document.getElementById("close")
+
 btn.addEventListener("click", () => {
   menuMobile.classList.toggle("ativo");
-
+  list.classList.toggle("ativo");
+  close.classList.toggle("ativo");
 });
