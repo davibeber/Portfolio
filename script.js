5e57c046eb4eec49d66f796464bb0efc0f1fbac1
Davi <davibeber.rbr@gmail.com>
2026-09-01 14:51:20 -0300

    feat: Adição do loop no carrossel do projeto em destaque

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const btnleft = document.getElementById("btnleft");
 const btnright = document.getElementById("btnright");
 
 
+// Função para abrir e fechar o menu mobile
+
 btn.addEventListener("click", () => {
   menuMobile.classList.toggle("ativo");
   list.classList.toggle("ativo");
@@ -21,8 +23,10 @@ menuMobile.addEventListener("click", () => {
     close.classList.toggle("ativo");
 
 });
+// Fim da função para abrir e fechar o menu mobile
 
 
+// Função de mover o carrossel para esquerda e direita
 btnleft.addEventListener("click", () =>{
   container.scrollBy({
     left: -container.clientWidth,
@@ -31,10 +35,40 @@ btnleft.addEventListener("click", () =>{
 });
 
 
-
 btnright.addEventListener("click", () =>{
   container.scrollBy({
     left:container.clientWidth,
     behavior: "smooth"
   })
 })
+// fim da função de mover o carrossel para esquerda e direita
+
+
+// Funçao para o carrossel "teletransportar" para a primeira ou ultima foto quando chegar no final ou inicio do carrossel
+window.addEventListener("load", () => {
+  container.style.scrollBehavior = "auto"; //Pulo sem animação
+  container.scrollLeft = container.clientWidth; //Avança a largura de 1 de foto (não entendi isso aqui)
+  container.style.scrollBehavior = "smooth"; // Ativa a animação
+});
+
+
+
+// 4. O MÁGICO "TELETRANSPORTE" INVISÍVEL
+container.addEventListener("scrollend", () => {
+  const itemWidth = container.clientWidth;
+  const maxScroll = container.scrollWidth - itemWidth;
+
+  // Se chegou no CLONE DA FOTO 1 (no final do carrossel)
+  if (Math.ceil(container.scrollLeft) >= maxScroll - 5) {
+    container.style.scrollBehavior = "auto"; // Desliga a animação suave
+    container.scrollLeft = itemWidth;         // Teleporta para a Foto 1 REAL
+    container.style.scrollBehavior = "smooth"; // Religam a animação suave
+  }
+
+  // Se chegou no CLONE DA ÚLTIMA FOTO (no início do carrossel)
+  if (container.scrollLeft <= 5) {
+    container.style.scrollBehavior = "auto"; // Desliga a animação suave
+    container.scrollLeft = maxScroll - itemWidth; // Teleporta para a Última Foto REAL
+    container.style.scrollBehavior = "smooth"; // Religam a animação suave
+  }
+});
