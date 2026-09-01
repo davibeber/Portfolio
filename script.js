b24b1d7052f9f00b215a4756bc6cf0f6a1bbb814
Davi <davibeber.rbr@gmail.com>
2026-09-01 19:25:39 -0300

    feat: finalizando apresentação de projeto em versao desktop

--- script.js
+++ script.js
@@ -47,14 +47,31 @@ btnright.addEventListener("click", () =>{
 // Funçao para o carrossel "teletransportar" para a primeira ou ultima foto quando chegar no final ou inicio do carrossel
 window.addEventListener("load", () => {
   container.style.scrollBehavior = "auto"; //Pulo sem animação
+// Tem coisa que não faz diferença mudar. testei instant, inherit, smooth e o pulo sem animação nao teve nada de diferente, então alguams coisas para funcionarem preciso de um "auto" como padrao, procede? 
+
   container.scrollLeft = container.clientWidth; //Avança a largura de 1 de foto (não entendi isso aqui)
+  // clientWidth é a largura visível do container, ou seja, a largura da foto. Então, ao definir scrollLeft como clientWidth, você está movendo o carrossel para a posição da segunda foto (a primeira foto real), pulando a primeira foto que é um clone da última foto. Isso cria o efeito de loop contínuo no carrossel.
   container.style.scrollBehavior = "smooth"; // Ativa a animação
 });
 
 
+// como explicar container.scrollLeft = container.clientWidth? 
+// mas pq tem scrollLeft mas não scrollRight?
+
 
 // 4. O MÁGICO "TELETRANSPORTE" INVISÍVEL
 container.addEventListener("scrollend", () => {
+
+  // o que é scrollend?
+
+
+// Basicamente aqui em baixo, foi criado duas constantes para renomear funções com outros nomes e ficar mais facil de manipular
+// elas identificam o momento exato em que estiver no clone da primeira ou da ultima foto
+// Igualam o scrollLeft com a imagem correspondente e troca sem animação
+// Tenho que aprender como fazer isso sem copiar
+// E como desenvolver o que está dentro do if, pois não entendi como itemwidth consegue identificar a primeira figura
+// faria sentido se por exemplo: figura 1 = itemwidth mas não é isso que ocorre
+
   const itemWidth = container.clientWidth;
   const maxScroll = container.scrollWidth - itemWidth;
 
