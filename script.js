f2a47b17773d64e1be6f7abf37f6dcde4bdeca8f
Davi <davibeber.rbr@gmail.com>
2026-08-31 22:00:31 -0300

    feat: adição do primeiro projeto em destaque

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 const btn =document.getElementById("btn");
-
 const menuMobile = document.getElementById("menu_mobile");
-
 const list = document.getElementById("list");
+const close = document.getElementById("close");
 
-const close = document.getElementById("close")
+const container = document.getElementById("carrossel_container"); 
+const btnleft = document.getElementById("btnleft");
+const btnright = document.getElementById("btnright");
+
 
 btn.addEventListener("click", () => {
   menuMobile.classList.toggle("ativo");
@@ -19,3 +21,20 @@ menuMobile.addEventListener("click", () => {
     close.classList.toggle("ativo");
 
 });
+
+
+btnleft.addEventListener("click", () =>{
+  container.scrollBy({
+    left: -container.clientWidth,
+    behavior:"smooth"
+  });
+});
+
+
+
+btnright.addEventListener("click", () =>{
+  container.scrollBy({
+    left:container.clientWidth,
+    behavior: "smooth"
+  })
+})
