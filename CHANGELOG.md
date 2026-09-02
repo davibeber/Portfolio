cececa331543c9222e38b5ac63ad1514ae001ba4
Davi <davibeber.rbr@gmail.com>
2026-09-02 16:40:19 -0300

    chore(release): v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,28 @@
 # Changelog
 
-Todas as mudanças notáveis neste projeto serão documentadas neste arquivo.
+Todas as mudanças neste projeto serão documentadas neste arquivo.
+
+## [v1.0.0] - 02/09/26
+
+### Lançamento Oficial
+- **Menu Mobile:** Implementadas funções de alternância e navegação dinâmica para o menu em telas menores.
+- **Canais de Contato:** Integração de links diretos para contato em outras redes sociais e projetos públicos
+
+### Estética & Experiência do Usuário 
+- **Ajustes Responsivos:** Ajuste visual final do layout e componentes para melhor experiência do usuário (UX) em dispositivos móveis
+
+### Publicação & Processo
+- **Deploy de Produção:** Estrutura final validada em ambiente de testes e pronta para publicação oficial via GitHub Pages.
 
 
-## [v 0.2.1] - 02/09/26
+
+## [v0.2.2] - 02/09/26
+
+- Alterando tamanho da fonte em alguns textos
+- Limpando estrutura do site de excesso de comentários
+
+
+## [v0.2.1] - 02/09/26
 
 ### Adicionado 
 
@@ -17,7 +36,7 @@ Todas as mudanças notáveis neste projeto serão documentadas neste arquivo.
 
 
 
-## [v 0.1.0] - 26/08/26
+## [v0.1.0] - 26/08/26
 
 Pela curiosidade de saber "se sites recebem um número de versão quando estão minimamente prontos", descobri a atividade de registrar notas de atualização (release notes / patch notes).
 
@@ -32,8 +51,4 @@ Pela curiosidade de saber "se sites recebem um número de versão quando estão 
 ### Configuração (Chore)
 - Configuração do repositório Git.
 - Deploy de testes: Uso de deploy inicialmente para testes em versões mobile, tanto na versão datada com nas anteriores.
-
-
-
-
 
--- script.js
+++ script.js
@@ -46,46 +46,29 @@ btnright.addEventListener("click", () =>{
 
 // Funçao para o carrossel "teletransportar" para a primeira ou ultima foto quando chegar no final ou inicio do carrossel
 window.addEventListener("load", () => {
-  container.style.scrollBehavior = "auto"; //Pulo sem animação
-// Tem coisa que não faz diferença mudar. testei instant, inherit, smooth e o pulo sem animação nao teve nada de diferente, então alguams coisas para funcionarem preciso de um "auto" como padrao, procede? 
-
+  container.style.scrollBehavior = "auto"; 
   container.scrollLeft = container.clientWidth; //Avança a largura de 1 de foto (não entendi isso aqui)
   // clientWidth é a largura visível do container, ou seja, a largura da foto. Então, ao definir scrollLeft como clientWidth, você está movendo o carrossel para a posição da segunda foto (a primeira foto real), pulando a primeira foto que é um clone da última foto. Isso cria o efeito de loop contínuo no carrossel.
-  container.style.scrollBehavior = "smooth"; // Ativa a animação
+  container.style.scrollBehavior = "smooth"; 
 });
-
-
-// como explicar container.scrollLeft = container.clientWidth? 
-// mas pq tem scrollLeft mas não scrollRight?
-
 
 // 4. O MÁGICO "TELETRANSPORTE" INVISÍVEL
 container.addEventListener("scrollend", () => {
-
-  // o que é scrollend?
-
-
-// Basicamente aqui em baixo, foi criado duas constantes para renomear funções com outros nomes e ficar mais facil de manipular
-// elas identificam o momento exato em que estiver no clone da primeira ou da ultima foto
-// Igualam o scrollLeft com a imagem correspondente e troca sem animação
-// Tenho que aprender como fazer isso sem copiar
-// E como desenvolver o que está dentro do if, pois não entendi como itemwidth consegue identificar a primeira figura
-// faria sentido se por exemplo: figura 1 = itemwidth mas não é isso que ocorre
 
   const itemWidth = container.clientWidth;
   const maxScroll = container.scrollWidth - itemWidth;
 
   // Se chegou no CLONE DA FOTO 1 (no final do carrossel)
   if (Math.ceil(container.scrollLeft) >= maxScroll - 5) {
-    container.style.scrollBehavior = "auto"; // Desliga a animação suave
+    container.style.scrollBehavior = "auto";
     container.scrollLeft = itemWidth;         // Teleporta para a Foto 1 REAL
-    container.style.scrollBehavior = "smooth"; // Religam a animação suave
+    container.style.scrollBehavior = "smooth";
   }
 
   // Se chegou no CLONE DA ÚLTIMA FOTO (no início do carrossel)
   if (container.scrollLeft <= 5) {
-    container.style.scrollBehavior = "auto"; // Desliga a animação suave
+    container.style.scrollBehavior = "auto";
     container.scrollLeft = maxScroll - itemWidth; // Teleporta para a Última Foto REAL
-    container.style.scrollBehavior = "smooth"; // Religam a animação suave
+    container.style.scrollBehavior = "smooth"; 
   }
 });
